2d829768b0718ed4f14b0c577610e4d0c539c7eb
Aidan Grimshaw <grimshawaidan@gmail.com>
2020-05-21 23:51:09 -0700

    fixed batch script

--- submit.bash
+++ submit.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH -J MatrixMul
+#SBATCH -J montecarloTemplate
 #SBATCH -A cs475-575
 #SBATCH -p class
 #SBATCH --gres=gpu:1
-#SBATCH -o matrixmul.out
-#SBATCH -e matrixmul.err
+#SBATCH -o montecarloTemplate.out
+#SBATCH -e montecarloTemplate.err
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=grimshaa@oregonstate.edu
 for t in 1 2 4 8 16 32
 do
-/usr/local/apps/cuda/cuda-10.1/bin/nvcc -DNUMT=$t -o matrixMul matrixMul.cu
-./matrixMul
+/usr/local/apps/cuda/cuda-10.1/bin/nvcc -DNUMT=$t -o montecarloTemplate montecarloTemplate.cu
+./montecarloTemplate
 done
