c460d125d6fb233818d2d1a37d1176619bb3af76
Aidan Grimshaw <grimshawaidan@gmail.com>
2020-05-21 23:31:17 -0700

    added sumbit script

---
+++ submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -J MatrixMult
+#SBATCH -A cs475-575
+#SBATCH -p class
+#SBATCH –gres=gpu:1
+#SBATCH -o matrixmul.out
+#SBATCH -e matrixmul.err
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=joeparallel@oregonstate.edu
+/usr/local/apps/cuda/cuda-10.1/bin/nvcc -o matrixMul matrixMul.cu
+./matrixMul
